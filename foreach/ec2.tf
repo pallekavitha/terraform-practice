53e8f996a5d39b335560def61b511160b340fe0e
pallekavitha <pallewarkavitha007@gmail.com>
2023-09-16 13:21:49 +0530

    edited file

--- foreach/ec2.tf
+++ foreach/ec2.tf
@@ -1,11 +1,11 @@
-# resource "aws_instance" "roboshop" {
-#    for_each = var.instances
-#    ami = var.ami_id
-#    instance_type = each.value
-#    tags = {
-#        Name = each.key 
-#    }
-# }
+resource "aws_instance" "roboshop" {
+    for_each = var.instances
+    ami = var.ami_id
+    instance_type = each.value
+    tags = {
+        Name = each.key 
+    }
+}
 
 # output "aws_instance_info" {
 #   value = aws_instance.roboshop
